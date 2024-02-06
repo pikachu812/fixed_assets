1c148cd03e73a10ee187eed52f222ab8e4afc838
Your Name <15821958312@163.com>
2024-02-06 17:49:50 +0800

    initial commit

---
+++ src/main/java/com/example/fixedassets/FixedassetsApplication.java
@@ -0,0 +1,13 @@
+package com.example.fixedassets;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class  FixedassetsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FixedassetsApplication.class, args);
+    }
+
+}
